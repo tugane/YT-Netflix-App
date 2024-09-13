b79fa8e84f26a1cd1d1fe478b12db7bd3ea337cc
tugane <tooamiel@gmail.com>
2024-09-13 09:31:49 +0200

    🛠️🛠️

---
+++ app/(tabs)/(home)/index.tsx
@@ -0,0 +1,15 @@
+import {Text, View} from 'react-native'
+import React from 'react'
+
+const HomeScreen: React.FC = () => {
+
+    return (
+        <View>
+            <Text>
+                HomeScreen
+            </Text>
+        </View>
+    )
+}
+
+export default HomeScreen
---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,28 @@
+import {Tabs} from 'expo-router';
+import Colors from "@/constants/Colors";
+
+export default function TabLayout() {
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors.tabIconSelected,
+                tabBarInactiveTintColor: Colors.tabIconDefault,
+                headerShown: false,
+                tabBarStyle: {
+                    position: "absolute",
+                    borderTopWidth: 0,
+                    backgroundColor: Colors.background
+                },
+            }}>
+            <Tabs.Screen
+                name="(home)"
+            />
+            <Tabs.Screen
+                name="newAndHot"
+            />
+            <Tabs.Screen
+                name="downloads"
+            />
+        </Tabs>
+    );
+}
